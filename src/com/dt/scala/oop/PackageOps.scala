976584a52da8aefc8c32297bae66177dec1ee753
swq <grxshen@hotmail.com>
2016-03-19 09:24:36 +0800

    init import

---
+++ src/com/dt/scala/oop/PackageOps.scala
@@ -0,0 +1,48 @@
+package com.dt.scala.oop
+
+
+//包的组织
+package object study {
+  val default_value = "111"
+}
+
+package study {
+
+class PP {
+  var name = default_value;
+}
+
+}
+
+package ai {
+
+package alarm {
+
+class JavaService {
+
+}
+
+trait OO {
+
+}
+
+}
+
+package dog {
+  class A{
+
+  }
+}
+
+
+}
+
+//包的导入
+import java.awt.{Color,Font}
+import java.util.{HashMap => JavaHashMap}
+import scala.{StringBuilder =>_}
+
+
+class PackageOps {
+
+}
